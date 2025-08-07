905aa0d412d8a9cab4e2820af0181add08fa3062
--show-origin <m.lekstan@interia.pl>
2025-08-07 10:08:00 +0200

    tanstack router - not working yet

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import svgr from "vite-plugin-svgr";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 const __dirname = import.meta.dirname;
 
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(), 
+    svgr(), 
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
